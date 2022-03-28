cf26d11a5cd71c3dd9aaa3208eef890e88cc5003
masashi kishimoto <drehbleistift@gmail.com>
2022-03-28 18:28:26 +0900

    中途半端

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,59 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import React, { useState, useRef } from 'react'
+import ReactCrop, {
+  centerCrop,
+  Crop,
+  PixelCrop,
+  makeAspectCrop,
+} from 'react-image-crop'
+
+const centerAspectCrop = (
+  mediaWidth: number,
+  mediaHeight: number,
+  aspect: number
+) => {
+  return centerCrop(
+    makeAspectCrop(
+      {
+        unit: '%',
+        width: 90,
+      },
+      aspect,
+      mediaWidth,
+      mediaHeight
+    ),
+    mediaWidth,
+    mediaHeight
+  )
+}
 
 const Home: NextPage = () => {
+  const [imgSrc, setImgSrc] = useState('')
+  const previewCanvasRef = useRef<HTMLCanvasElement>(null)
+  const imgRef = useRef<HTMLImageElement>(null)
+  const [crop, setCrop] = useState<Crop>()
+  const [completedCrop, setCompletedCrop] = useState<PixelCrop>()
+  const [aspect, setAspect] = useState<number | undefined>(16 / 9)
+
+  const onSelectFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setCrop(undefined)
+      const reader = new FileReader()
+      reader.addEventListener('load', () => {
+        setImgSrc(reader.result?.toString() || '')
+      })
+      reader.readAsDataURL(e.target.files[0])
+    }
+  }
+
+  const onImageLoad = (e: React.SyntheticEvent<HTMLImageElement>) => {
+    if (aspect) {
+      const { width, height } = e.currentTarget
+      setCrop(centerAspectCrop(width, height, aspect))
+    }
+  }
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
@@ -10,6 +62,19 @@ const Home: NextPage = () => {
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-xl font-bold">ちょっとまって</h1>
+        <div>
+          <input type="file" accept="image/*" onChange={onSelectFile} />
+        </div>
+        {Boolean(imgSrc) && (
+          <ReactCrop
+            crop={crop}
+            onChange={(_, percentCrop) => setCrop(percentCrop)}
+            onComplete={(c) => setCompletedCrop(c)}
+            aspect={aspect}
+          >
+            <img ref={imgRef} alt="Crop me" src={imgSrc} onLoad={onImageLoad} />
+          </ReactCrop>
+        )}
       </main>
     </div>
   )
