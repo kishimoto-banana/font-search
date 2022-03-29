71085122ae6cf58f9a79e21a1761389c96c0d785
masashi kishimoto <drehbleistift@gmail.com>
2022-03-29 17:18:35 +0900

    とりあえずreact-cropperに入れ替える

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,77 +1,36 @@
+import 'cropperjs/dist/cropper.css'
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import React, { useRef, useState } from 'react'
-import ReactCrop, {
-  centerCrop,
-  Crop,
-  makeAspectCrop,
-  PixelCrop,
-} from 'react-image-crop'
-import 'react-image-crop/dist/ReactCrop.css'
+import React, { useState } from 'react'
+import { Cropper } from 'react-cropper'
 import ImageUploader from '../components/imageUploader'
-import { canvasPreview } from '../lib/canvasPreview'
-import { useDebounceEffect } from '../lib/useDebounceEffect'
-
-const centerAspectCrop = (
-  mediaWidth: number,
-  mediaHeight: number,
-  aspect: number
-) => {
-  return centerCrop(
-    makeAspectCrop(
-      {
-        unit: '%',
-        width: 90,
-      },
-      aspect,
-      mediaWidth,
-      mediaHeight
-    ),
-    mediaWidth,
-    mediaHeight
-  )
-}
 
 const Home: NextPage = () => {
-  const [imgSrc, setImgSrc] = useState('')
-  const previewCanvasRef = useRef<HTMLCanvasElement>(null)
-  const imgRef = useRef<HTMLImageElement>(null)
-  const [crop, setCrop] = useState<Crop>()
-  const [completedCrop, setCompletedCrop] = useState<PixelCrop>()
-  const [aspect, setAspect] = useState<number | undefined>(undefined)
+  const [image, setImage] = useState('')
+  const [cropData, setCropData] = useState('#')
+  const [cropper, setCropper] = useState<any>()
 
   const onSelectFile = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files.length > 0) {
-      setCrop(undefined)
       const reader = new FileReader()
       reader.addEventListener('load', () => {
-        setImgSrc(reader.result?.toString() || '')
+        setImage(reader.result?.toString() || '')
       })
       reader.readAsDataURL(e.target.files[0])
     }
   }
 
-  const onImageLoad = (e: React.SyntheticEvent<HTMLImageElement>) => {
-    if (aspect) {
-      const { width, height } = e.currentTarget
-      setCrop(centerAspectCrop(width, height, aspect))
-    }
-  }
+  // const getCropData = () => {
+  //   if (typeof cropper !== 'undefined') {
+  //     setCropData(cropper.getCroppedCanvas().toDataURL())
+  //   }
+  // }
 
-  useDebounceEffect(
-    async () => {
-      if (
-        completedCrop?.width &&
-        completedCrop.height &&
-        imgRef.current &&
-        previewCanvasRef.current
-      ) {
-        canvasPreview(imgRef.current, previewCanvasRef.current, completedCrop)
-      }
-    },
-    100,
-    [completedCrop]
-  )
+  // const onCrop = () => {
+  //   const imageElement: any = cropperRef?.current
+  //   const cropper: any = imageElement?.cropper
+  //   console.log(cropper.getCroppedCanvas().toDataURL())
+  // }
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
@@ -80,32 +39,37 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <ImageUploader onChange={onSelectFile} />
-
-        {Boolean(imgSrc) && (
-          <ReactCrop
-            crop={crop}
-            onChange={(_, percentCrop) => setCrop(percentCrop)}
-            onComplete={(c) => setCompletedCrop(c)}
-            // aspect={aspect}
-          >
-            <img ref={imgRef} alt="Crop me" src={imgSrc} onLoad={onImageLoad} />
-          </ReactCrop>
-        )}
         <div>
-          {Boolean(completedCrop) && (
-            <canvas
-              ref={previewCanvasRef}
-              style={{
-                border: '1px solid black',
-                objectFit: 'contain',
-                width: completedCrop?.width,
-                height: completedCrop?.height,
-              }}
-            />
-            // <CropResult ref={previewCanvasRef} crop={completedCrop} />
-          )}
+          <ImageUploader onChange={onSelectFile} />
+          <Cropper
+            src={image}
+            style={{ height: 400, width: '100%' }}
+            zoomTo={0.5}
+            initialAspectRatio={1}
+            viewMode={1}
+            minCropBoxHeight={10}
+            minCropBoxWidth={10}
+            background={false}
+            responsive={true}
+            autoCropArea={1}
+            checkOrientation={false}
+            onInitialized={(instance) => {
+              setCropper(instance)
+            }}
+            preview=".img-preview"
+            guides={true}
+          />
         </div>
+
+        {Boolean(image) && (
+          <div>
+            <h1>Preview</h1>
+            <div
+              className="img-preview"
+              style={{ width: '100%', height: 400 }}
+            />
+          </div>
+        )}
       </main>
     </div>
   )
