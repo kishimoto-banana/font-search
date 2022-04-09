b63d90f8cb44742fe8f0a8e6333f18b819672ed4
masashi kishimoto <drehbleistift@gmail.com>
2022-04-09 17:21:26 +0900

    プラポリとか削除

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,22 +3,6 @@ const Footer = () => {
     <footer className="mt-8 flex w-full flex-col items-center justify-center gap-2 border-t">
       <div className="flex flex-row flex-wrap gap-8 pt-2">
         <a
-          href=""
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-sm hover:underline"
-        >
-          利用規約
-        </a>
-        <a
-          href=""
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-sm hover:underline"
-        >
-          プライバシーポリシー
-        </a>
-        <a
           href="https://twitter.com/unpuy_tw"
           target="_blank"
           rel="noopener noreferrer"
