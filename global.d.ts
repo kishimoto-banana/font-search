c834706d6db975bfb56149a4786c865c047a0483
masashi kishimoto <drehbleistift@gmail.com>
2022-04-09 17:40:22 +0900

    ga

--- global.d.ts
+++ global.d.ts
@@ -5,5 +5,6 @@ declare namespace NodeJS {
     readonly NEXT_PUBLIC_FONT_SEARCH_API_KEY: string
     readonly NEXT_PUBLIC_VISION_API_ENDPOINT: string
     readonly NEXT_PUBLIC_VISION_API_KEY: string
+    readonly NEXT_PUBLIC_GOOGLE_ANALYTICS_ID: string
   }
 }
