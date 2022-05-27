929704d72adeeb6274542cf650d204a341f69188
masashi kishimoto <drehbleistift@gmail.com>
2022-05-27 16:30:53 +0900

    typo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -47,7 +47,7 @@ export default function Document() {
           content="https://fontpint.com/favicons/ogp.png"
         />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="https://fontpint.comk" />
+        <meta name="twitter:site" content="https://fontpint.com" />
         <meta
           name="twitter:image"
           content="https://fontpint.com/favicons/ogp.png"
